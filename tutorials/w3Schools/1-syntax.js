9dad6ca033032b435e98c7b7c299a0ea95c7ba29
Hayley <hb@hungrybeargaming.co.uk>
2023-04-19 10:58:46 +0100

    JSON: Syntax

---
+++ tutorials/w3Schools/1-syntax.js
@@ -0,0 +1,9 @@
+person = {name:"John", age:31, city:"New York"}
+//you can access a persons name like this
+person.name;
+//it can also be accessed like this
+person["name"];
+//data can be modified like this
+person.name = "Gilbert"
+//it can also be modified like this
+person["name"] = Gilbert
